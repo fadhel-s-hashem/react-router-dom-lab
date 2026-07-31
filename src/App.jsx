c9decacf665c2e03d299237ab0c35bd155b42a93
Fadhel <fadhel.s.hashem@gmail.com>
2026-07-31 12:46:37 +0300

    navbar basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 // src/App.jsx
+import NavBar from "./components/NavBar";
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  return (
+    <>
+    <h1>Hello world!</h1>
+    <NavBar/>
+
+
+    </>
+  )
+
 };
 
 export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router"
+
+const NavBar = () => {
+    return(
+        <div>
+        <Link to='/'>Home</Link>
+            
+            
+        </div>
+
+
+    )
+
+}
+
+export default NavBar
